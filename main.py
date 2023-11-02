df5b483a0acd9b9d3f8977c0ce5a1e64b7317f2d
Ashley Stewart <a.stewart.au@gmail.com>
2023-11-02 13:10:05 +1000

    QSMxT: v6.3.2

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python
+
 # set up environment
 print("[INFO] Importing Python modules")
+
 import json
 import os
 import shutil
 import glob
-from scripts.qsmxt_functions import sys_cmd
+from qsmxt.scripts.qsmxt_functions import sys_cmd
 
 # create necessary directories
 print("[INFO] Creating directories...")
+
 work_dir = os.path.abspath("work_dir")
 in_dir = os.path.join(work_dir, "0_inputs")
 bids_dir = os.path.join(work_dir, "1_bids")
@@ -24,17 +28,23 @@ print("[INFO] Loading configuration...")
 with open('config.json') as config_json_file_handle:
 	config_json = json.load(config_json_file_handle)
 
-with open(config_json['phase-json'][0]) as phase_json_file_handle:
-	phase_json = json.load(phase_json_file_handle)
-	TE = phase_json['EchoTime']
-	TE_idx = 1 if 'EchoNumber' not in phase_json else int(phase_json['EchoNumber'])
-	if (('EchoTrainLength' in phase_json and int(phase_json['EchoTrainLength']) > 1) or
-	   ('EchoNumber' in phase_json and int(phase_json['EchoTrainLength']) > 1)):
-		suffix = 'MEGRE'
-		file_pattern = "sub-1_ses-1_run-1_echo-{TE_idx}_part-{part}_{suffix}.{ext}"
-	else:
-		suffix = 'T2starw'
-		file_pattern = "sub-1_ses-1_run-1_part-{part}_{suffix}.{ext}"
+# Check if all keys exist
+keys = ['mag', 'phase', 'mag-json', 'phase-json']
+if not all(key in config_json for key in keys):
+    raise KeyError("Not all required keys found in the configuration.")
+
+# Check if all lengths are equal
+lengths = [len(config_json[key]) for key in keys]
+if not all(length == lengths[0] for length in lengths):
+    raise RuntimeError("The number of input files must be equal for 'mag', 'phase', 'mag-json', and 'phase-json'.")
+
+# setup filepattern
+if lengths[0] == 1:
+	suffix = 'T2starw'
+	file_pattern = "sub-1_ses-1_run-1_part-{part}_{suffix}.{ext}"
+else:
+	suffix = 'MEGRE'
+	file_pattern = "sub-1_ses-1_run-1_echo-{TE_idx}_part-{part}_{suffix}.{ext}"
 
 # TODO: Make this better! This information should probably be given by the brainlife datatype
 #       because JSON headers tend to be unreliable!
@@ -54,21 +64,22 @@ if suffix == 'T2starw':
 		json.dump(mag_json, mag_json_file_handle)
 
 # Load into variables predefined code inputs
-mag_nii_path = os.path.abspath(config_json['mag'][0])
-phs_nii_path = os.path.abspath(config_json['phase'][0])
-mag_json_path = os.path.abspath(config_json['mag-json'][0])
-phs_json_path = os.path.abspath(config_json['phase-json'][0])
+for i in range(lengths[0]):
+	mag_nii_path = os.path.abspath(config_json['mag'][i])
+	phs_nii_path = os.path.abspath(config_json['phase'][i])
+	mag_json_path = os.path.abspath(config_json['mag-json'][i])
+	phs_json_path = os.path.abspath(config_json['phase-json'][i])
 
-print("[INFO] Copying files to NIfTI directory...")
-shutil.copy(mag_nii_path, os.path.join(in_dir, file_pattern.format(TE_idx=TE_idx, part="mag", suffix=suffix, ext="nii")))
-shutil.copy(phs_nii_path, os.path.join(in_dir, file_pattern.format(TE_idx=TE_idx, part="phase", suffix=suffix, ext="nii")))
-shutil.copy(mag_json_path, os.path.join(in_dir, file_pattern.format(TE_idx=TE_idx, part="mag", suffix=suffix, ext="json")))
-shutil.copy(phs_json_path, os.path.join(in_dir, file_pattern.format(TE_idx=TE_idx, part="phase", suffix=suffix, ext="json")))
+	print("[INFO] Copying files to NIfTI directory...")
+	shutil.copy(mag_nii_path, os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part="mag", suffix=suffix, ext="nii")))
+	shutil.copy(phs_nii_path, os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part="phase", suffix=suffix, ext="nii")))
+	shutil.copy(mag_json_path, os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part="mag", suffix=suffix, ext="json")))
+	shutil.copy(phs_json_path, os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part="phase", suffix=suffix, ext="json")))
 
-print("[INFO] Running run_1_niftiConvert.py...")
-sys_cmd(cmd=f"run_1_niftiConvert.py {in_dir} {bids_dir} --t2starw_protocol_patterns '*' --auto_yes")
-print("[INFO] Running run_2_qsm.py")
-sys_cmd(cmd=f"run_2_qsm.py {bids_dir} {qsm_dir} --premade {config_json['premade']} --auto_yes")
+print("[INFO] Running nifti-convert...")
+sys_cmd(cmd=f"niftic-convert {in_dir} {bids_dir} --qsm_protocol_patterns '*' --auto_yes")
+print("[INFO] Running qsmxt")
+sys_cmd(cmd=f"qsmxt {bids_dir} {qsm_dir} --premade {config_json['premade']} --auto_yes")
 
 qsm_files = glob.glob(os.path.join(qsm_dir, "qsm_final", "**", "*"))
 
