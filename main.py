3edff179084a51dd2c17081e7de97365ceb61572
Ashley Stewart <a.stewart.au@gmail.com>
2024-03-22 13:05:04 +1000

    Update

--- main.py
+++ main.py
@@ -34,29 +34,32 @@ print("[INFO] Loading configuration...")
 with open('config.json') as config_json_file_handle:
 	config_json = json.load(config_json_file_handle)
 
-if 'bids' in config_json:
-	print(f"[INFO] BIDS input detected! Extracting {config_json['bids']} to {bids_dir}.")
-	sys_cmd(f"tar xf {config_json['bids']} -C {bids_dir}")
+# Check if all required keys exist
+keys = ['mag', 'phase', 'mag-json', 'phase-json']
+if not all(key in config_json for key in keys):
+	raise KeyError("Not all required keys found in the configuration.")
 
+# Check if all lengths are equal
+lengths = [len(config_json[key]) for key in keys]
+if not all(length == lengths[0] for length in lengths):
+	raise RuntimeError("The number of input files must be equal for 'mag', 'phase', 'mag-json', and 'phase-json'.")
+
+# Check length of fourth dimension
+length_4d = 1
+with open(config_json['mag-json'][0]) as json_file:
+	json_data = json.load(json_file)
+	if 'echoes' in json_data:
+		length_4d = len(json_data['echoes'])
+
+# setup filepattern
+if lengths[0] == 1 and length_4d == 1:
+	suffix = 'T2starw'
+	file_pattern = "sub-1_ses-1_run-1_part-{part}_{suffix}.{ext}"
 else:
-	# Check if all required keys exist
-	keys = ['mag', 'phase', 'mag-json', 'phase-json']
-	if not all(key in config_json for key in keys):
-		raise KeyError("Not all required keys found in the configuration.")
+	suffix = 'MEGRE'
+	file_pattern = "sub-1_ses-1_run-1_echo-{TE_idx}_part-{part}_{suffix}.{ext}"
 
-	# Check if all lengths are equal
-	lengths = [len(config_json[key]) for key in keys]
-	if not all(length == lengths[0] for length in lengths):
-		raise RuntimeError("The number of input files must be equal for 'mag', 'phase', 'mag-json', and 'phase-json'.")
-
-	# setup filepattern
-	if lengths[0] == 1:
-		suffix = 'T2starw'
-		file_pattern = "sub-1_ses-1_run-1_part-{part}_{suffix}.{ext}"
-	else:
-		suffix = 'MEGRE'
-		file_pattern = "sub-1_ses-1_run-1_echo-{TE_idx}_part-{part}_{suffix}.{ext}"
-
+if length_4d == 1:
 	# TODO: Make this better! This information should probably be given by the brainlife datatype
 	#       because JSON headers tend to be unreliable!
 	if suffix == 'T2starw':
@@ -91,11 +94,39 @@ else:
 		print(f"[INFO] {phs_json_path} -> {os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part='phase', suffix=suffix, ext='json'))}")
 		shutil.copy(phs_json_path, os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part='phase', suffix=suffix, ext='json')))
 		print(f"[INFO] Input directory {in_dir} contains: {os.listdir(in_dir)}")
+else:
+	# Split json files
+	with open(config_json['mag-json'][0]) as json_file:
+		json_data = json.load(json_file)
+		for i in range(len(json_data['echoes'])):
+			with open(os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part='mag', suffix=suffix, ext='json')), 'w') as mag_json_file:
+				mag_json_file.write(json.dumps(json_data['echoes'][i]))
+	with open(config_json['phase-json'][0]) as json_file:
+		json_data = json.load(json_file)
+		for i in range(len(json_data['echoes'])):
+			with open(os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part='phase', suffix=suffix, ext='json')), 'w') as mag_json_file:
+				mag_json_file.write(json.dumps(json_data['echoes'][i]))
+			
+	# Split nifti files
+	mag_nii = nib.load(config_json['mag'][0])
+	phase_nii = nib.load(config_json['phase'][0])
 
+	# Get the total number of volumes
+	num_volumes = mag_nii.shape[3]
 
-	print("[INFO] Running nifti-convert...")
-	sys_cmd(cmd=f"nifti-convert {in_dir} {bids_dir} --auto_yes", print_output=True, raise_exception=True)
-	sys_cmd(cmd=f"nifti-convert {in_dir} {bids_dir} --auto_yes", print_output=True, raise_exception=True)
+	# Loop over each volume index
+	for i in range(num_volumes):
+		# Get the 3D image for the current volume
+		mag_3d = mag_nii.slicer[..., i]
+		phase_3d = phase_nii.slicer[..., i]
+
+		# Save the 3D image to the desired location
+		nib.save(mag_3d, os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part='mag', suffix=suffix, ext='nii')))
+		nib.save(phase_3d, os.path.join(in_dir, file_pattern.format(TE_idx=i+1, part='phase', suffix=suffix, ext='nii')))
+
+print("[INFO] Running nifti-convert...")
+sys_cmd(cmd=f"nifti-convert {in_dir} {bids_dir} --auto_yes", print_output=True, raise_exception=True)
+sys_cmd(cmd=f"nifti-convert {in_dir} {bids_dir} --auto_yes", print_output=True, raise_exception=True)
 
 print("[INFO] Running qsmxt")
 sys_cmd(cmd=f"qsmxt {bids_dir} {qsm_dir} --premade {config_json['premade']} --auto_yes", print_output=True, raise_exception=True)
@@ -106,7 +137,4 @@ if len(qsm_files) == 0: raise Exception(f"No QSM files found in output directory
 print("[INFO] Copying QSM files to output directory...")
 for qsm_file in qsm_files:
 	shutil.copy(qsm_file, os.path.join(out_dir, "qsm.nii"))
-
-
-	
 
