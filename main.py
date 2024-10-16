e3fca74ebaa201283629a554767bae0c103727e3
Ashley Stewart <a.stewart.au@gmail.com>
2024-10-16 10:01:36 -0500

    Better error handling and correctly moving output sidecar

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import os
 import shutil
 import uuid
 import glob
-import nibabel as nib
 
 from qsmxt.scripts.sys_cmd import sys_cmd
 
@@ -37,72 +36,77 @@ if cli_params:
 
 # Check if all required keys exist
 print("[INFO] Checking required keys...")
-keys = ['phase', 'magnitude']
-if not all(key in config_json for key in keys):
-    raise KeyError("Not all required keys found in the configuration.")
+if not all(key in config_json for key in ['phase', 'magnitude']):
+    raise KeyError("Missing required keys: 'phase' or 'magnitude'")
+
+print("[INFO] Checking for subjects...")
+subjects = list({subject for input_entry in config_json.get('_inputs', []) if input_entry.get('meta', {}).get("subject")})
+if len(subjects) > 1:
+    raise RuntimeError("Multiple subjects found! This App can only process one at a time.")
+
+# random subject ID in case no subject identifier is available
+subject_rand = str(uuid.uuid4()).replace("-", "")
 
 # Base directory: assuming this is the parent directory where the "task_id" directories are located
 base_directory = os.path.abspath("..")
 
-# choose a random subject name for any data without a subject identifier
-subject_rand = str(uuid.uuid4()).replace("-", "")
-
-# Iterate over each element in the _inputs array
+# find and collect file names and metadata
+mag_files = []
+phs_files = []
 for input_entry in config_json.get('_inputs', []):
-    # Check if the "id" is "inputs"
     if input_entry.get('id') in ['magnitude', 'phase']:
         task_id = input_entry.get('task_id')
         subdir = input_entry.get('subdir')
-
-        # Construct the full path to the t2starw.nii file
         file_path = os.path.join(base_directory, task_id, subdir, 't2starw.nii')
-
-        # Ensure the file exists
         if not os.path.isfile(file_path):
-            print(f"[WARNING] File not found: {file_path}")
-            continue
+            raise RuntimeError(f"File {file_path} specified by config.json not found!")
 
-        # Fetch required fields for renaming
         meta = input_entry.get("meta", {})
+        tags = input_entry.get("tags", [])
+        datatype_tags = input_entry.get("datatype_tags", [])
         subject = meta.get("subject") or subject_rand
-
-        # Fetch optional fields
         session = meta.get("session")
         echo = meta.get("echo")
         suffix = "MEGRE" if echo else "T2starw"
 
-        # Determine whether the file is magnitude or phase based on tags
-        tags = input_entry.get("tags", []) + input_entry.get("datatype_tags", [])
-        if "part-mag" in tags:
+        if "part-mag" in datatype_tags:
             part = "mag"
-        elif "part-phase" in tags:
+            mag_files.append((file_path, subject, session, echo, part, suffix, meta))
+        elif "part-phase" in datatype_tags:
             part = "phase"
+            phs_files.append((file_path, subject, session, echo, part, suffix, meta))
         else:
             raise ValueError(f"Missing required 'part-mag' or 'part-phase' tag in input entry: {input_entry}")
 
-        # Construct the new filename based on available fields
-        new_filename_base = f"sub-{subject}"
-        if session:
-            new_filename_base += f"_ses-{session}"
-        if echo:
-            new_filename_base += f"_echo-{echo}"
-        new_filename_base += f"_part-{part}_{suffix}"
+if len(mag_files) != len(phs_files):
+    raise RuntimeError(f"Number of magnitude files ({len(mag_files)}) must equal number of phase files ({len(phs_files)})!")
 
-        # Construct the NIfTI destination path
-        nii_dest_path = os.path.join(in_dir, new_filename_base + ".nii")
+# Second loop: Copy the files and write the JSON metadata
+for file_info in mag_files + phs_files:
+    file_path, subject, session, echo, part, suffix, meta = file_info
 
-        # Copy the NIfTI file to the destination path
-        print(f"[INFO] Copying {file_path} to {nii_dest_path}")
-        shutil.copy(file_path, nii_dest_path)
+    # Construct the new filename based on available fields
+    new_filename_base = f"sub-{subject}"
+    if session:
+        new_filename_base += f"_ses-{session}"
+    if echo:
+        new_filename_base += f"_echo-{echo}"
+    new_filename_base += f"_part-{part}_{suffix}"
 
-        # Construct the JSON destination path
-        json_dest_path = os.path.join(in_dir, new_filename_base + ".json")
+    # Construct the NIfTI destination path
+    nii_dest_path = os.path.join(in_dir, new_filename_base + ".nii")
 
-        # Write the meta data to the destination JSON file
-        print(f"[INFO] Writing meta data to {json_dest_path}")
-        with open(json_dest_path, 'w') as json_file:
-            json.dump(meta, json_file, indent=4)
+    # Copy the NIfTI file to the destination path
+    print(f"[INFO] Copying {file_path} to {nii_dest_path}")
+    shutil.copy(file_path, nii_dest_path)
 
+    # Construct the JSON destination path
+    json_dest_path = os.path.join(in_dir, new_filename_base + ".json")
+
+    # Write the meta data to the destination JSON file
+    print(f"[INFO] Writing meta data to {json_dest_path}")
+    with open(json_dest_path, 'w') as json_file:
+        json.dump(meta, json_file, indent=4)
 
 print("[INFO] Running nifti-convert...")
 sys_cmd(cmd=f"nifti-convert {in_dir} {bids_dir} --auto_yes", print_output=True, raise_exception=True)
@@ -115,8 +119,11 @@ print("[INFO] Collecting outputs...")
 qsm_files_with_session = glob.glob(os.path.join(qsm_dir, "sub-*", "ses-*", "anat", "*Chimap.nii*"))
 qsm_files_without_session = glob.glob(os.path.join(qsm_dir, "sub-*", "anat", "*Chimap.nii*"))
 qsm_files = qsm_files_with_session + qsm_files_without_session
+
 if len(qsm_files) == 0:
-    raise RuntimeError(f"No QSM files found in output directory {os.path.join(qsm_dir, 'qsm')}")
+    raise RuntimeError(f"No QSM files found in output directory {os.path.join(qsm_dir, 'qsm')}!")
+if len(qsm_files) > 1:
+    raise RuntimeError(f"Multiple QSM files found in output directory {os.path.join(qsm_dir, 'qsm')}!")
 
 print("[INFO] Copying QSM files to output directory...")
 for qsm_file in qsm_files:
@@ -124,5 +131,8 @@ for qsm_file in qsm_files:
         sys_cmd(f"gunzip {qsm_file}", print_output=True, raise_exception=True).strip()
         qsm_file = qsm_file[:-3]
 
+    print(f"[INFO] Copying {qsm_file} to {os.path.join(out_dir, f'qsm.nii')}")
     shutil.copy(qsm_file, os.path.join(out_dir, f"qsm.nii"))
+
+shutil.copy(sorted(phs_files)[0].replace('.nii.gz', '.nii').replace('.nii', '.json'), os.path.join(out_dir, f"qsm.json"))
 
